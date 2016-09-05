dec8f4b25b2f4f1214e8a914166757d4e6ad7a11
mart93 <artfaulit@gmail.com>
2016-09-05 04:58:53 +0300

    fourth test commit

---
+++ content.php
@@ -0,0 +1,4 @@
+<?php
+    sleep(2);
+    echo "Данные: ".$_POST['name']." and ".$_POST['age']." years";
+?>
--- js/common.js
+++ js/common.js
@@ -1,1 +1,48 @@
+$(document).ready(function(){
 
+    /*=============================get===============================*/
+
+    $('#getForecast').bind('click', function(){
+
+        $.get('getForecast.txt', function(data){
+            $('.result').html(data);
+        });
+
+    });
+
+    /*==============================post==============================*/
+
+    $('#getForecast-2').bind('click', function(){
+
+        $.post('getForecast.php', function(data){
+            $('.result').html(data);
+        });
+
+    });
+
+    /*==============================ajax==============================*/
+
+    function funcBefore () {
+        $('#getUser').text('Loading...');
+    }
+
+    function funcSuccess (data) {
+        $('.result').html(data);
+    }
+
+    $('#getUser').bind('click', function(){
+
+        var user = 'Artemis';
+
+        $.ajax({
+            url:'content.php',
+            type:'POST',
+            data: ({name:user, age:22}),
+            dataType: 'HTML',
+            beforeSend: funcBefore,
+            success: funcSuccess
+        });
+
+    });
+
+});
